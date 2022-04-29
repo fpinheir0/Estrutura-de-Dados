7d03b478cf1e01b81b0c01b2222c887ee405caea
fepinheir0 <f.pinheir0@outlook.com>
2022-04-29 17:45:06 -0300

    Pilhas, por enquanto somente ponteiros e variaveis e menu

---
+++ Pilhas/pilhas.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node{
+ int num;
+ struct Node *prox;
+};
+typedef struct Node node;
+
+int tam;
+
+int menu(void);
+void inicia(node *PILHA);
+void opcao(node *PILHA, int op);
+void exibe(node *PILHA);
+void libera(node *PILHA);
+void push(node *PILHA);
+node *pop(node *PILHA);
+
+
+int main(void)
+{
+ node *PILHA = (node *) malloc(sizeof(node));
+ if(!PILHA){
+  printf("Sem memoria disponivel!\n");
+  exit(1);
+ }else{
+ inicia(PILHA);
+ int opt;
+
+ do{
+  opt=menu();
+  opcao(PILHA,opt);
+ }while(opt);
+
+ free(PILHA);
+ return 0;
+ }
+}
+
+void inicia(node *PILHA)
+{
+ PILHA->prox = NULL;
+ tam=0;
+}
+
