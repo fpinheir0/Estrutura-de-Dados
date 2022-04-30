458a2a343d64e68bd9ed123185d24c314fbc8241
fepinheir0 <f.pinheir0@outlook.com>
2022-04-29 21:35:55 -0300

    criando funções e apontando para a pilha alguns if e repetições para incrementar numeros na pilha.

--- Pilhas/pilhas.c
+++ Pilhas/pilhas.c
@@ -101,3 +101,27 @@ node *aloca()
     return novo;
     }
 }
+
+void exibe(node *PILHA){
+    if(vazia(PILHA)){
+        printf("PILHA vazia!\n\n");
+    return ;
+}
+
+    node *tmp;
+    tmp = PILHA->prox;
+        printf("PILHA:");
+    while( tmp != NULL){
+        printf("%5d", tmp->num);
+    tmp = tmp->prox;
+}
+        printf("\n        ");
+    int count;
+    for(count=0 ; count < tam ; count++)
+        printf("  ^  ");
+        printf("\nOrdem:");
+    for(count=0 ; count < tam ; count++)
+        printf("%5d", count+1);
+
+        printf("\n\n");
+}
