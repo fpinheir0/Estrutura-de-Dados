8cb82153f4e5596a08b881e84d8f5e504b9f9790
fepinheir0 <f.pinheir0@outlook.com>
2022-05-04 12:19:04 -0300

    inserindo comentario

--- ArvoreBinaria/arvore_binaria.c
+++ ArvoreBinaria/arvore_binaria.c
@@ -11,18 +11,18 @@ typedef struct arvore{
 	No *raiz;
 }Arvore;
 
-No* cria_no(int num);
-Arvore* cria_arvore(int num);
-void insere_no(No *tree, int num);
-void pesquisa_no(No *tree, int num);
-No* acha_menor(No *tree);
-int acha_maior(int a, int b);
-No *remove_no(No *tree, int num);
-int altura(No *tree);
-void exibir_emordem(No *tree);
-void exibir_preordem(No *tree);
-void exibir_posordem(No *tree);
-void deletar_arv(No *tree);
+// No* cria_no(int num);
+// Arvore* cria_arvore(int num);
+// void insere_no(No *tree, int num);
+// void pesquisa_no(No *tree, int num);
+// No* acha_menor(No *tree);
+// int acha_maior(int a, int b);
+// No *remove_no(No *tree, int num);
+// int altura(No *tree);
+// void exibir_emordem(No *tree);
+// void exibir_preordem(No *tree);
+// void exibir_posordem(No *tree);
+// void deletar_arv(No *tree);
 
 No* cria_no(int num){
 	No *tree;
